56ae0fdfb43de3d894435c07d544939cc3cb9de3
Rodrigar <rodriadriangarcia1997@gmail.com>
2023-08-07 11:30:25 -0300

    2

--- proyecto/apps/porfolio/views.py
+++ proyecto/apps/porfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .models import Proyecto
 def home(request):
-    return render(request,"apps/templates/profolio/home.html") 
+    proyectos = Proyecto.objects.all()
+    return render(request,"profolio/home.html",{"proyectos": proyectos})
--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'porfolio'
+    'porfolio',
+    'apps.porfolio.models.Proyecto'
 ]
 
 MIDDLEWARE = [
